30d01419b68e0e53ab8322492d658ff980956b7a
vixuowis <yinghuix@qq.com>
2014-10-07 14:18:40 +0800

    Add homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HipimsdataRails::Application.config.secret_key_base = '5c424118656f80204002a8fb9ea9b696611c437e90bc7d3a8b37be4fe0026a0510a925efa8d90662f937d9ce9a8bde4bb294fb0f52b9850eae8446c07bac0c7c'
