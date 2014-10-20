2d5dff0ec5b74a324b52b3a72cdc2e457e56cdfe
vixuowis <yinghuix@qq.com>
2014-10-21 00:58:06 +0800

    Add data table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
+    @hipims_data = [
+      [
+        1,"Ti","Ti",
+        "Ti靶",500,0.4,0.2,
+        nil,nil,nil,nil,
+        65,nil,nil,nil
+      ],
+      [
+        2,"Ti","Ti",
+        "Ti靶",550,1.1,0.6,
+        nil,nil,nil,nil,
+        65,nil,nil,nil
+      ],
+      [
+        3,"Ti","Ti",
+        "Ti靶",600,2,1.1,
+        nil,nil,nil,nil,
+        65,nil,nil,nil
+      ]
+    ]
   end
 end
