cf5b18b63efed0b84a448d8dfba32f283c2061cd
vixuowis <yinghuix@qq.com>
2014-10-30 01:31:50 +0800

    Add more controller actions on items

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 HipimsdataRails::Application.routes.draw do
+
   # get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :items
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
