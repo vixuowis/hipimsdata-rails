a6e9c4b0cc19c43592f84ddc162f301ab12397e7
lemonlinger <lemonlinger@gmail.com>
2014-11-04 00:48:08 +0800

    abstract more on col2 elem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 HipimsdataRails::Application.config.session_store :cookie_store, key: '_hipimsdata-rails_session'
+
+HipimsdataRails::Application.config.secret_key_base = '65422c087ad8271608dac56ef863dcc2ccd92a3c6fb3bd9020f1650b7beada03173c8c67745dbce4ae95705cfeef75343d1d8ddb2cfee18ef0ce127cda680f49'
