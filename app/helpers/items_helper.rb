39abbff507a5bb3fb1370c2d226460ba1a27342b
VIXUOWIS <vixuowis@hotmail.com>
2014-11-07 02:46:01 +0800

    Add more time parser

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -6,20 +6,31 @@ module ItemsHelper
   def vix_time(time)
     begin
       time = time.localtime
+      case time.hour
+        when 0..2 then daystep = "深夜"
+        when 3..5 then daystep = "凌晨"
+        when 6..8 then daystep = "早晨"
+        when 9..11 then daystep = "上午"
+        when 12..14 then daystep = "中午"
+        when 15..17 then daystep = "下午"
+        when 18..20 then daystep = "傍晚"
+        when 21..23 then daystep = "晚上"
+      end
+      
       if time.year == Time.now.year
         this_date = time.to_date
         today = Time.now.localtime.to_date
         if this_date == today
-          time.strftime("今天 %T")
+          time.strftime("今天#{daystep} %T")
         elsif this_date == today.yesterday
-          time.strftime("昨天 %T")
+          time.strftime("昨天#{daystep} %T")
         elsif this_date == today.yesterday.yesterday
-          time.strftime("前天 %T")
+          time.strftime("前天#{daystep} %T")
         else
-          time.strftime("%m-%d %T")
+          time.strftime("%m-%d #{daystep} %T")
         end
       else
-        time.strftime("%F %T")
+        time.strftime("%F #{daystep} %T")
       end
     rescue
       nil
